4fffa29c848ccc5b98c1b0d76aaf13f9b5d44f20
Sr Jengibre <spobleteobando@gmail.com>
2026-06-26 20:07:33 -0400

    tarea

---
+++ fundamentos-web/js/clase-9/script.js
@@ -0,0 +1,40 @@
+// function saludar() {
+//     console.log("¡Hola, amigo!");
+// }
+
+// saludar();
+
+
+// function saludar(nombreAmigo) {
+//     console.log("Hola Amigo!", nombreAmigo);
+// }
+
+// saludar("Elías");
+
+// function sumar(numeroUno, numeroDos) {
+//     console.log("La suma es:", numeroUno + numeroDos);
+// }
+
+// sumar(2.5, 4);
+// sumar(3, 6);
+
+
+// //Acá tenemos la función con las instrucciones
+// function encontrarMaximo(a, b) {
+//     if (a > b) {
+//         return a;
+//     } else {
+//         return b;
+//     }
+// }
+
+// //Valor de los dos números a comparar
+// let numero1 = 10;
+// let numero2 = 7;
+
+// //Llamamos a la función y le pasamos los números
+// //var maximo recibe el número mayor
+// let maximo = encontrarMaximo(numero1, numero2);
+
+// //Vemos por consola los números
+// console.log("El máximo entre", numero1, "y", numero2, "es:", maximo);
