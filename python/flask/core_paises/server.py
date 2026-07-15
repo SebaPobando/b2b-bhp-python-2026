48c3cee0fce20875cb9a0f7f8c8bfbb9cfdadbf2
Sr Jengibre <spobleteobando@gmail.com>
2026-07-15 19:28:36 -0400

    core paises

---
+++ python/flask/core_paises/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hola_mundo():
+    return '¡Hola Mundo!'
+
+@app.route('/paises')
+def lista_paises():
+    paises = [
+   {'pais': 'Argentina' , 'capital': 'Buenos Aires'},
+   {'pais': 'Brasil' , 'capital': 'Brasilia'},
+   {'pais': 'Chile' , 'capital': 'Santiago de Chile'},
+   {'pais': 'Colombia' , 'capital': 'Bogotá'},
+   {'pais': 'Costa Rica' , 'capital': 'San José'},
+   {'pais': 'Paraguay' , 'capital': 'Asunción'},
+   {'pais': 'Perú' , 'capital': 'Lima'}
+]
+    return render_template('paises.html', paises = paises)
+if __name__ == '__main__':
+    app.run(debug=True)
--- python/flask/hola_flask/server.py
+++ python/flask/hola_flask/server.py
@@ -1,65 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def hola_mundo():
-#    return '¡Hola Mundo!'
-
-# @app.route('/chao')
-# def chao_mundo():
-#     return '¡Chao Mundo!'
-
-# @app.route('/exito')
-# def exito():
-#    return "¡Éxito!"
-
-# @app.route('/saludo/<nombre>')
-# def saludo(nombre):
-#    print(nombre)
-#    return (f'¡Hola {nombre}!')
-
-# @app.route('/color/<nombre>/<color>')
-# def color_favorito(nombre, color):
-#    print(nombre)
-#    print(color)
-#    return f'Hola {nombre}, tu color favorito es el {color}'
-
-# @app.route('/saludo/<nombre>/<int:num>')
-# def hola_cantidad(nombre, num):
-#    return f'¡Hola {nombre}!'*num
-
-@app.route('/')
-def hola_flask():
-    return '¡Hola desde Flask!'
-    
-@app.route('/ruta')
-def ruta():
-    return '¿Qué ruta estás buscando?'
-
-@app.route('/bienvenido/python')
-def bienvenido_python():
-    return 'Bienvenid@ a esta ruta Python'
-
-@app.route('/bienvenido/miyagi')
-def bienvenido_miyagi():
-    return 'Bienvenid@ a esta ruta Miyagi'
-
-@app.route('/bienvenido/taquito')
-def bienvenido_taquito():
-    return 'Bienvenid@ a esta ruta Taquito'
-
-@app.route('/repite/<int:num>/hola')
-def repite_hola(num):
-    return 'Repite después de mi: ' + ' hola'*num
-    
-@app.route('/repite/<int:num1>/adios')
-def repite_adios(num1):
-    return 'Repite después de mi:' + ' adios'*num1
-
-@app.route('/repite/<int:num2>/divertido')
-def repite_divertido(num2):
-    return 'Repite después de mi:' + ' divertido'*num2
+@app.route('/listas')
+def renderizar_listas():
+   listado_estudiantes = [
+       {
+          'nombre': 'Florencia', 
+          'edad': 25
+          },
+       {
+          'nombre': 'Valentina', 
+          'edad': 30
+          },
+       {
+          'nombre': 'José', 
+          'edad': 27
+          },
+       {
+          'nombre': 'Patricio', 
+          'edad': 21
+          }
+   ]
+   return render_template('listas.html', numeros=[7, 15, 22], estudiantes=listado_estudiantes)
 
 if __name__=="__main__":
    app.run(debug=True)
